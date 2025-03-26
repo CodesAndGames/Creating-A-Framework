0272449408d30ba5c92d9fc1a08d430ed4237665
JaxDanger <jax@jaxdanger.com>
2025-03-26 17:52:22 -0400

    part 6

--- core/server/modules/player.lua
+++ core/server/modules/player.lua
@@ -3,7 +3,6 @@ local player = class('player')
 function player:__construct()
 	self.config = module('config') -- loads config
 
-	self.players = {} -- active players on the server
 	self.characters = {} -- characters created by player
 	self.cData = {} -- active character's data
 
@@ -12,10 +11,12 @@ end
 
 function player:addPlayer(id, data)
 	if not (id or data) or type(data) ~= 'table' then
-		return Framework.log('Unable to add player due to invalid data. '..tostring(id), 'error')
+		return Framework.log('Unable to create character due to invalid data. '..tostring(id), 'error')
 	end
 
 	id = tostring(id)
+	local char_id = data.character_id
+	print('adding player for id', id, char_id)
 
 	if not (data.firstname or data.lastname) then -- mandatory data for player
 		return Framework.log('Player is missing their first and last name!', 'error')
@@ -45,30 +46,69 @@ function player:addPlayer(id, data)
 		data.gender = 'unknown'
 	end
 
-	if not self.players[id] then
-		self.players[id] = data
-		Framework.log('Create player successfully', 'info')
-	end
+	self.characters[id] = self.characters[id] or {}
+	self.characters[id][char_id] = data
 
-	print(json.encode(self.players[id], {indent = true}))
+	local identifier = GetPlayerIdentifierByType(id, 'fivem')
+	local params = {
+		['@identifier'] = identifier,
+		['@cData'] = json.encode(self.characters[id])
+	}
+
+	local isInDb = Framework:execute('SelectPlayer', {['@identifier'] = identifier})
+	if not isInDb[1] then
+		local isAdded = Framework:execute('AddToPlayers', params)
+		if isAdded.affectedRows > 0 then
+			print('add to players was successful')
+			return 'success'
+		else
+			print('add to players was NOT successful.')
+		end
+	end
+	local result = Framework:execute('SavePlayer', params)
+	if result.affectedRows > 0 then
+		return 'success'
+	else
+		error('could not create character.')
+		return false
+	end
 end
 
 function player:removePlayer(id, reason)
 	id = tostring(id)
-	if self.players[id] then
+	if self.cData[id] then
 		local success = self:save(id)
 		if success then
-			self.players[id] = nil
+			self.cData[id] = nil
 			Framework.log('Player '..id..' has left the server.\n		Reason for removal: '..reason..'.', 'info')
 		end
 	end
 end
 
-function player:save(id)
-	local identifier = GetPlayerIdentifierByType(id, 'fivem')
+
+function player:select(id, char_id)
 	id = tostring(id)
-	if self.players[id] then
+	if self.characters[id] then
+		if self.characters[id][char_id] then
+			self.cData[id] = self.characters[id][char_id]
+			return 'ok'
+		else
+			print('char_id not found: '..char_id)
+		end
+	else
+		print('no characters found for id ', id, char_id)
+	end
+end
+
+
+
+
+function player:save(id)
+	id = tostring(id)
+	local identifier = GetPlayerIdentifierByType(id, 'fivem')
+	if self.cData[id] then
 		Framework.log('Saving player(s)...','info')
+		local char_id = self.cData[id].character_id
 
 		-- Save to database
 		local isInDb = Framework:execute('SelectPlayer', {
@@ -76,7 +116,7 @@ function player:save(id)
 		})
 		if isInDb[1] then
 			local isSave = Framework:execute('SavePlayer', {
-				['cData'] = json.encode(self.players[id]),
+				['cData'] = json.encode(self.characters[id]),
 				['identifier'] = identifier
 			})
 			if isSaved then
@@ -84,10 +124,7 @@ function player:save(id)
 				return true
 			end
 		else
-			Framework:execute('AddToPlayers', {
-				['@identifier'] = identifier,
-				['cData'] = json.encode(self.players[id])
-			})
+			Framework.log('Unable to save '..id..'\'s character. Not in db')
 		end
 	end
 	return false
@@ -96,8 +133,8 @@ end
 
 function player:getPlayer(id)
 	id = tostring(id)
-	if self.players[id] then
-		return self.players[id]
+	if self.cData[id] then
+		return self.cData[id]
 	end
 end
 
@@ -107,13 +144,13 @@ function player:addMoney(id, amount, account) -- self:addMoney(1, 500, 'cash')
 	end
 
 	id = tostring(id)
-	if self.players[id] then
+	if self.cData[id] then
 		if account == 'cash' then
-			self.players[id].bank.cash = self.players[id].bank.cash + amount
+			self.cData[id].bank.cash = self.cData[id].bank.cash + amount
 		elseif account == 'checking' then
-			self.players[id].bank.checking.balance = self.players[id].bank.checking.balance + amount
+			self.cData[id].bank.checking.balance = self.cData[id].bank.checking.balance + amount
 		elseif account == 'savings' then
-			self.players[id].bank.savings.balance = self.players[id].bank.savings.balance + amount
+			self.cData[id].bank.savings.balance = self.cData[id].bank.savings.balance + amount
 		else
 			Framework.log('Cannot add moeny. Invalid account type.', 'error')
 		end
@@ -128,18 +165,62 @@ function player:removeMoney(id, amount, account) -- self:removeMoney(1, 500, 'ca
 	end
 
 	id = tostring(id)
-	if self.players[id] then
+	if self.cData[id] then
 		if account == 'cash' then
-			self.players[id].bank.cash = self.players[id].bank.cash - amount
+			self.cData[id].bank.cash = self.cData[id].bank.cash - amount
 		elseif account == 'checking' then
-			self.players[id].bank.checking.balance = self.players[id].bank.checking.balance - amount
+			self.cData[id].bank.checking.balance = self.cData[id].bank.checking.balance - amount
 		elseif account == 'savings' then
-			self.players[id].bank.savings.balance = self.players[id].bank.savings.balance - amount
+			self.cData[id].bank.savings.balance = self.cData[id].bank.savings.balance - amount
 		else
 			Framework.log('Cannot add moeny. Invalid account type.', 'error')
 		end
 		return
 	end
+end
+
+function player:getAllCharacters(id)
+	id = tostring(id)
+
+	if self.characters[id] then
+		return self.characters[id]
+	end
+
+	local identifier = GetPlayerIdentifierByType(id, 'fivem')
+	local result = Framework:execute('SelectPlayer', {['@identifier'] = identifier})
+	if result[1] and result[1].cData then
+		local decoded = json.decode(result[1].cData)
+		if decoded = then
+			self.characters[id] = decoded
+			print(json.encode(decoded))
+			return decoded
+		else
+			Framework.log('Couldn to decode cData for player: '..identifier, 'error')
+		end
+	end
+end
+
+function player:getCharactersForUI(id)
+	id = tostring(id)
+
+	local characters = self.characters[id]
+	local formatted = {}
+
+	for key, char in pairs(characters or {}) do
+		local slot = tonumber(key:match("^(%d+)%-%w+")) -- extracts the slot number (1,2 or 3)
+		if slot then
+			formatted[#formatted + 1] = {
+				slot = slot,
+				firstname = char.firstname,
+				lastname = char.lastname, 
+				age = char.age,
+				-- gender = char.gender,
+				-- character_id = char.character_id
+				-- add more data to display in UI
+			}
+		end
+	end
+	return formatted
 end
 
 Framework:RegisterModule('player', player)
--- core/sh_framework.lua
+++ core/sh_framework.lua
@@ -4,6 +4,11 @@ Framework.modules = {}
 function Framework:__construct()
 	print('Framework has started.')
 	self.queries = {}
+	self.pendingCallbacks = {}
+	self.callbacks = {
+		client = {},
+		server = {}
+	}
 end
 
 function Framework.log(msg, level)
@@ -57,4 +62,83 @@ function Framework:execute(name, data)
 	return result
 end
 
+
+local function generateEventID()
+	return "cb_"..tostring(math.random(1000000, 9999999))
+end
+
+function Framework:RegisterCallback(eventName, cb)
+	if IsDuplicityVersion() then
+		self.callbacks.server[eventName] = cb
+	else
+		self.callbacks.client[eventName] = cb
+	end
+end
+
+function Framework:TriggerCallback(eventName, callback, target, ...)
+	local isServer = IsDuplicityVersion()
+	local args = { ... }
+
+	if isServer then
+		local cb = self.callbacks.server[eventName]
+		if cb then
+			local result = target and cb(target, table.unpack(args)) or cb(source, table.unpack(args))
+			if callback then callback(result) end
+			return
+		end
+	else
+		local cb = self.callbacks.client[eventName]
+		if cb then
+			local result = cb(table.unpack(args))
+			if callback then callback(result) end
+			return
+		end
+	end
+
+	local eventID = generateEventID()
+	self.pendingCallbacks[eventID] = callback
+
+	if isServer then
+		if not target then
+			Framework.log('Missing target player id for callback: '.. eventName)
+			return
+		end
+
+		TriggerClientEvent('framework:triggerClient', target, eventName, eventID, table.unpack(args))
+	else
+		TriggerServerEvent('framework:triggerServer', eventName, eventID, table.unpack(args))
+	end
+end
+
+if IsDuplicityVersion() then
+	RegisterNetEvent('framework:triggerServer', function(eventName, eventID, ...)
+		local src = source
+		local cb = Framework.callbacks.server[eventName]
+		if cb then
+			local result(src, ...)
+			TriggerClientEvent('framework:serverResponse',src, eventID, result)
+		else
+			Framework.log('Missing server callback for '..eventName)
+		end
+	end)
+end
+
+if not IsDuplicityVersion() then
+	RegisterNetEvent('framework:triggerClient', function(eventName, eventID, ...)
+		local cb =Framework.callbacks.client[eventName]
+		if cb then
+			local result = cb(...)
+			TriggerServerEvent('framework:serverResponse', eventID, result)
+		else
+			Framework.log('Missing client callback for: '..eventName)
+		end
+	end)
+
+	RegisterNetEvent('framework:serverResponse', function(eventID, result)
+		if Framework.pendingCallbacks[eventID] then
+			Framework.pendingCallbacks[eventID](result)
+			Framework.pendingCallbacks[eventID] = nil
+		end
+	end)
+end
 return Framework
