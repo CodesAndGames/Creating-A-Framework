5e2ae6263139a147e484d4c95f5a2262d99ad456
JaxDanger <jax@jaxdanger.com>
2025-04-03 19:46:28 -0400

    moved hud build folder, updated fxmanifest

--- hud/fxmanifest.lua
+++ hud/fxmanifest.lua
@@ -1,18 +1,16 @@
 fx_version 'cerulean'
-
-games {"gta5", "rdr3"}
+game "gta5"
 
 author "Project Error"
 version '1.0.0'
-
 lua54 'yes'
 
-ui_page 'web/build/index.html'
+ui_page 'build/index.html'
 
 client_script "client/**/*"
 server_script "server/**/*"
 
 files {
-  'web/build/index.html',
-  'web/build/**/*'
+  'build/index.html',
+  'build/**/*'
 }
